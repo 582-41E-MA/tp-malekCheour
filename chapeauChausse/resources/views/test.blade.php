925af755db7ea8d45f109c0d28136f5b226a4d2a
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-05-06 14:36:34 -0400

    Ajouter le component Panier (version statique)

--- chapeauChausse/resources/views/test.blade.php
+++ chapeauChausse/resources/views/test.blade.php
@@ -1,13 +1,16 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-</head>
+@extends('partials.base')
+
+@section('titre', trans('Pricing'))
+
+@section('content')
+
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<div class="mt-24">
+  
+  <main class="max-w-6xl mx-auto pt-10 pb-36 px-8">
+  <x-panier>Cart</x-panier>
+    
+</main>
 </body>
+@endsection
 </html>
