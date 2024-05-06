14e9c356cd52e69825477db5e0f8cb8464036f47
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-05-06 14:28:28 -0400

    Ajouter les dépendences reliées à tailwind

--- chapeauChausse/routes/web.php
+++ chapeauChausse/routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('test');
+});
+
 
 //Route pour les produits
 Route::get('/produits', [ProduitController::class, 'index']);
