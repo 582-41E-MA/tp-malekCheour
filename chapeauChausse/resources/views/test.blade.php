48ff888d825d3af4e0a1b455735ac37fe20e7ce0
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-05-06 15:25:26 -0400

    Ajouter module tailwind, nos couleurs dans test.blade

--- chapeauChausse/resources/views/test.blade.php
+++ chapeauChausse/resources/views/test.blade.php
@@ -9,6 +9,33 @@
   
   <main class="max-w-6xl mx-auto pt-10 pb-36 px-8">
   <x-panier>Cart</x-panier>
+ <div class='bg-clr-fonce-900 mt-2.5 py-1'>clr-fonce-900</div>
+<div class='bg-clr-fonce-800 mt-2.5 py-2'>clr-fonce-800</div>
+<div class='bg-clr-fonce-700 mt-2.5 py-2'>clr-fonce-700</div>
+<div class='bg-clr-fonce-600 mt-2.5 py-2'>clr-fonce-600</div>
+<div class='bg-clr-fonce-500 mt-2.5 py-2'>clr-fonce-500</div>
+<div class='bg-clr-fonce-400 mt-2.5 py-2'>clr-fonce-400</div>
+<div class='bg-clr-fonce-300 mt-2.5 py-2'>clr-fonce-300</div>
+<div class='bg-clr-fonce-200 mt-2.5 py-2'>clr-fonce-200</div>
+<div class='bg-clr-fonce-100 mt-2.5 py-2'>clr-fonce-100</div>
+<div class='bg-clr-mid-900 mt-2.5 py-2'>clr-mid-900</div>
+<div class='bg-clr-mid-800 mt-2.5 py-2'>clr-mid-800</div>
+<div class='bg-clr-mid-700 mt-2.5 py-2'>clr-mid-700</div>
+<div class='bg-clr-mid-600 mt-2.5 py-2'>clr-mid-600</div>
+<div class='bg-clr-mid-500 mt-2.5 py-2'>clr-mid-500</div>
+<div class='bg-clr-mid-400 mt-2.5 py-2'>clr-mid-400</div>
+<div class='bg-clr-mid-300 mt-2.5 py-2'>clr-mid-300</div>
+<div class='bg-clr-mid-200 mt-2.5 py-2'>clr-mid-200</div>
+<div class='bg-clr-mid-100 mt-2.5 py-2'>clr-mid-100</div>
+<div class='bg-clr-pale-900 mt-2.5 py-2'>clr-pale-900</div>
+<div class='bg-clr-pale-800 mt-2.5 py-2'>clr-pale-800</div>
+<div class='bg-clr-pale-700 mt-2.5 py-2'>clr-pale-700</div>
+<div class='bg-clr-pale-600 mt-2.5 py-2'>clr-pale-600</div>
+<div class='bg-clr-pale-500 mt-2.5 py-2'>clr-pale-500</div>
+<div class='bg-clr-pale-400 mt-2.5 py-2'>clr-pale-400</div>
+<div class='bg-clr-pale-300 mt-2.5 py-2'>clr-pale-300</div>
+<div class='bg-clr-pale-200 mt-2.5 py-2'>clr-pale-200</div>
+<div class='bg-clr-pale-100 mt-2.5 py-2'>clr-pale-100</div>
     
 </main>
 </body>
--- chapeauChausse/tailwind.config.js
+++ chapeauChausse/tailwind.config.js
@@ -1,13 +1,55 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+      extend: {
+        colors: {
+          "clr-fonce": {
+            900: "#1e1e1e",
+            800: "#373737",
+            700: "#505050",
+            600: "#646464",
+            500: "#797979",
+            400: "#8e8e8e",
+            300: "#a3a3a3",
+            200: "#b9b9b9",
+            100: "#cfcfcf",
+            50: "#e5e5e5",
+          },
+          "clr-mid": {
+            900: "#53311d",
+            800: "#6c4127",
+            700: "#855234",
+            600: "#9d6544",
+            500: "#b37958",
+            400: "#c78f70",
+            300: "#d8a68b",
+            200: "#e7bea9",
+            100: "#f2d8ca",
+            50: "#fbf2ed",
+          },
+          "clr-pale": {
+              900: "#453720",
+              800: "#5b492b",
+              700: "#705b39",
+              600: "#866e49",
+              500: "#9b835d",
+              400: "#af9874",
+              300: "#c3ae8f",
+              200: "#d5c4ac",
+              100: "#e7dbcc",
+              50: "#f7f3ee",
+          },
+          "clr-danger": "#834E2F",
+          "clr-warning": "#E2B385",
+        },
+      },
   },
-  plugins: [],
-}
+  plugins: [
+  ],
+};
 
